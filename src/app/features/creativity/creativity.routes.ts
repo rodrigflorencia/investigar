be0b6c4a081320af108b532a714f7b005d6ef3a5
Flor Rodriguez <rodrig.florencia@gmail.com>
2025-08-15 14:44:41 -0300

    remove unused

--- src/app/features/creativity/creativity.routes.ts
+++ src/app/features/creativity/creativity.routes.ts
@@ -2,48 +2,41 @@ import { Routes } from '@angular/router';
 import { CreativityTestPage } from './components/creativity-test.component';
 import { CreativityInstructionsPage } from './components/creativity-instructions.component';
 import { PersonalInfoComponent } from './components/creativity-personal-info.component';
-import { MessageOkPrevTestComponent } from '../../shared/components/message-ok-prev-test.component';
 
 export const CREATIVITY_ROUTES: Routes = [
-  { 
-    path: '', 
-    children: [
-      { 
-        path: '', 
-        redirectTo: 'instructions', 
-        pathMatch: 'full' 
-      },
-      { 
-        path: 'instructions', 
-        component: CreativityInstructionsPage,
-        data: { title: 'Instrucciones',
-            nextRoute: 'creativity-personal-info'  }
-      },
-      { 
-        path: 'personal-info',
-        component: PersonalInfoComponent,
-        data: { 
-          title: 'Datos Personales',
-          nextRoute: '/creativity/test' 
-        }
-      },
-      {
-        path: 'message-ok',
-        component: MessageOkPrevTestComponent,
-        data: { 
-          title: '¡Listo!',
-          nextRoute: '/creativity/test' 
-        }
-      },
-      { 
-        path: 'test', 
-        component: CreativityTestPage,
-        data: { title: 'Prueba de Creatividad' }
-      },
-      { 
-        path: '**', 
-        redirectTo: 'instructions' 
-      }
-    ]
-  }
+    {
+        path: '',
+        children: [
+            {
+                path: '',
+                redirectTo: 'instructions',
+                pathMatch: 'full'
+            },
+            {
+                path: 'instructions',
+                component: CreativityInstructionsPage,
+                data: {
+                    title: 'Instrucciones',
+                    nextRoute: 'creativity-personal-info'
+                }
+            },
+            {
+                path: 'personal-info',
+                component: PersonalInfoComponent,
+                data: {
+                    title: 'Datos Personales',
+                    nextRoute: '/creativity/test'
+                }
+            },
+            {
+                path: 'test',
+                component: CreativityTestPage,
+                data: { title: 'Prueba de Creatividad' }
+            },
+            {
+                path: '**',
+                redirectTo: 'instructions'
+            }
+        ]
+    }
 ];
